72577761c6ca6801049d17d5a6f67df709ca32ff
gtechgithub <36880236+gtechgithub@users.noreply.github.com>
2019-09-09 11:34:35 +0800

    Update EmployeeValidator.java

--- src/main/java/com/javapoint/EmployeeValidator.java
+++ src/main/java/com/javapoint/EmployeeValidator.java
@@ -21,4 +21,20 @@ public class EmployeeValidator implements Validator{
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "email.required");
 		
 	}
+	
 }
+
+/*****
+
+public void validate(Object obj, Errors e) {
+        ValidationUtils.rejectIfEmpty(e, "name", "name.empty");
+        Person p = (Person) obj;
+        if (p.getAge() < 0) {
+            e.rejectValue("age", "negativevalue");
+        } else if (p.getAge() > 110) {
+            e.rejectValue("age", "too.darn.old");
+        }
+    }
+
+
+***/
